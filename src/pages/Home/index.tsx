bf29d43ae52276c419ee3c8a0023762ccb41b180
Samanta Fluture <samantafluture@gmail.com>
2022-09-18 13:39:45 -0400

    custom styles for table of history component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,13 +15,26 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">I will work on</label>
-          <TaskInput id="task" placeholder="Give your project a name" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Give your project a name"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Project 1" />
+            <option value="Project 2" />
+            <option value="Project 3" />
+          </datalist>
 
           <label htmlFor="minutesAmount">during</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutes.</span>
